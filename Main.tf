e4b929a577406c4c22188c932b2269c7ce8ce86c
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-19 00:20:35 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -24,4 +24,57 @@ resource "aws_s3_bucket" "example-val" {
   }
 }
 
+resource "aws_iam_role" "lambda" {
+  name = "lambda-s3-access"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "s3:*",
+            "Resource": "*"
+        }
+    ]
+}
+
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "autoscaling:Describe*",
+                "cloudwatch:*",
+                "logs:*",
+                "sns:*",
+                "iam:GetPolicy",
+                "iam:GetPolicyVersion",
+                "iam:GetRole"
+            ],
+            "Effect": "Allow",
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "iam:CreateServiceLinkedRole",
+            "Resource": "arn:aws:iam::*:role/aws-service-role/events.amazonaws.com/AWSServiceRoleForCloudWatchEvents*",
+            "Condition": {
+                "StringLike": {
+                    "iam:AWSServiceName": "events.amazonaws.com"
+                }
+            }
+        }
+    ]
+}
+EOF
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+
+
+
+
 

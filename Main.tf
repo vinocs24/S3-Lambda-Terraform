ece1a9377d0656cd632745903b3cb13942e2242e
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-18 16:18:01 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,5 +1,21 @@
+# Use AWS Terraform provider
+provider "aws" {
+  region = "us-west-2"
+}
+
+# Create S3 Bucket
 resource "aws_s3_bucket" "b" {
   bucket = "my-tf-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "b1" {
+  bucket = "my-tf-test-bucket1"
   acl    = "private"
 
   tags = {

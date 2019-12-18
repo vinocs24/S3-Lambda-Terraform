35fe215fccbf6f6924ec92fbf533f4aeaed8d476
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-18 23:01:34 +0530

    Create Variable.tf

---
+++ Variable.tf
@@ -0,0 +1,9 @@
+variable "source-bucket-name" {
+  description = "Source Bucket Name"
+  default     = "my-tf-test-bucket-dev"
+}
+
+variable "destination-bucket-name" {
+  description = "Destination Bucket Name"
+  default     = "my-tf-test-bucket-val"
+}

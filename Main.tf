11ca58527b49b1c6c326d980b0918b46078cc99e
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-19 00:29:41 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -24,48 +24,22 @@ resource "aws_s3_bucket" "example-val" {
   }
 }
 
-resource "aws_iam_role" "lambda" {
-  name = "lambda-s3-access"
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
 
   assume_role_policy = <<EOF
 {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": "s3:*",
-            "Resource": "*"
-        }
-    ]
-}
-
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": [
-                "autoscaling:Describe*",
-                "cloudwatch:*",
-                "logs:*",
-                "sns:*",
-                "iam:GetPolicy",
-                "iam:GetPolicyVersion",
-                "iam:GetRole"
-            ],
-            "Effect": "Allow",
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": "iam:CreateServiceLinkedRole",
-            "Resource": "arn:aws:iam::*:role/aws-service-role/events.amazonaws.com/AWSServiceRoleForCloudWatchEvents*",
-            "Condition": {
-                "StringLike": {
-                    "iam:AWSServiceName": "events.amazonaws.com"
-                }
-            }
-        }
-    ]
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
 }
 EOF
 }

28c77e0a2250fafe54559b5c77c4b1f139078e91
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-19 00:52:38 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -23,8 +23,8 @@ resource "aws_iam_role" "test_role" {
 EOF
 }
 
-resource "aws_iam_role_policy" "test_policy" {
-  name = "test_policy"
+resource "aws_iam_role_policy" "s3_policy" {
+  name = "s3-fullaccess"
   role = "${aws_iam_role.test_role.id}"
 
   policy = <<EOF
@@ -42,6 +42,44 @@ resource "aws_iam_role_policy" "test_policy" {
 }
 EOF
 }
+
+resource "aws_iam_role_policy" "CW_policy" {
+  name = "CW-fullaccess"
+  role = "${aws_iam_role.test_role.id}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "autoscaling:Describe*",
+                "cloudwatch:*",
+                "logs:*",
+                "sns:*",
+                "iam:GetPolicy",
+                "iam:GetPolicyVersion",
+                "iam:GetRole"
+            ],
+            "Effect": "Allow",
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "iam:CreateServiceLinkedRole",
+            "Resource": "arn:aws:iam::*:role/aws-service-role/events.amazonaws.com/AWSServiceRoleForCloudWatchEvents*",
+            "Condition": {
+                "StringLike": {
+                    "iam:AWSServiceName": "events.amazonaws.com"
+                }
+            }
+        }
+    ]
+}
+EOF
+}
+
+
 
 # Create S3 Bucket
 resource "aws_s3_bucket" "example-dev" {

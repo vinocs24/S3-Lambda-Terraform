5129e3266ebaa3bdc3b10e361d625648d7a846ea
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-19 15:15:40 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -29,12 +29,12 @@ data "aws_caller_identity" "current" {}
 
 resource "aws_cloudtrail" "foobar" {
   name                          = "tf-trail-foobar"
-  s3_bucket_name                = aws_s3_bucket.foo-dev.id
+  s3_bucket_name                = aws_s3_bucket.vino1990.id
   s3_key_prefix                 = "prefix"
   include_global_service_events = false
 }
 
-resource "aws_s3_bucket" "foo-dev" {
+resource "aws_s3_bucket" "vino1990" {
   bucket        = "tf-test-trail"
   force_destroy = true
 

b0ef6611faaf499ee83c1ec84facade0531a2fd9
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-19 15:10:50 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 
-# Create S3 Bucket
+/*# Create S3 Bucket
 resource "aws_s3_bucket" "example-dev" {
   bucket = var.source-bucket-name
   acl    = "private"
@@ -23,10 +23,51 @@ resource "aws_s3_bucket" "example-val" {
     Name        = "My bucket- Val"
     Environment = "vel"
   }
+}*/
+
+
+
+resource "aws_cloudtrail" "foobar" {
+  name                          = "tf-trail-foobar"
+  s3_bucket_name                = aws_s3_bucket.foo.id
+  s3_key_prefix                 = "prefix"
+  include_global_service_events = false
 }
 
+resource "aws_s3_bucket" "foo" {
+  bucket        = "tf-test-trail"
+  force_destroy = true
 
-
-
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AWSCloudTrailAclCheck",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:GetBucketAcl",
+            "Resource": "arn:aws:s3:::tf-test-trail"
+        },
+        {
+            "Sid": "AWSCloudTrailWrite",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:PutObject",
+            "Resource": "arn:aws:s3:::tf-test-trail/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+            "Condition": {
+                "StringEquals": {
+                    "s3:x-amz-acl": "bucket-owner-full-control"
+                }
+            }
+        }
+    ]
+}
+POLICY
+}
 
 

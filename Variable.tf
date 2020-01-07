098cccf74a630f29247deafa4629e4e84a194803
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2020-01-07 12:12:54 +0530

    Update Variable.tf

--- Variable.tf
+++ Variable.tf
@@ -7,3 +7,15 @@ variable "destination-bucket-name" {
   description = "Destination Bucket Name"
   default     = "my-tf-test-bucket-val"
 }
+
+variable "function_name" {
+  default = "minimal_lambda_function"
+}
+
+variable "handler" {
+  default = "lambda.handler"
+}
+
+variable "runtime" {
+  default = "python3.6"
+}

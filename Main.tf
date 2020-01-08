b1697e9f3b8bca2cf006f284ad5ca6f2b22e165d
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2020-01-08 16:54:37 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -47,7 +47,7 @@ EOF
 } 
 
 
-resource "aws_iam_policy" "policy-s3" {
+resource "aws_iam_policy" "policy" {
   name        = "s3-move-file"
   description = "s3-move-file"
 
@@ -90,8 +90,8 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "test-attach" {
-  role       = ["aws_iam_role.iam_for_lambda.name"]
-  policy_arn = "aws_iam_policy.policy-s3.arn"
+  role       = "aws_iam_role.iam_for_lambda.name"
+  policy_arn = "aws_iam_policy.policy.arn"
 }
 
 

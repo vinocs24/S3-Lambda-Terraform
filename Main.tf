8689cbe056738bb6400a68432e537f3c68d5a958
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2020-01-08 12:32:34 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -26,27 +26,6 @@ resource "aws_s3_bucket" "example-val" {
 }
 
 
-resource "aws_iam_role" "iam_for_lambda" {
-  name = "iam_for_lambda"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
-
 # Archive a single file.
 
 data "archive_file" "Test" {

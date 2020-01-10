902f9657fa5cec1274c2f9a6158fcd3199313db5
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2020-01-10 12:08:59 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -26,7 +26,7 @@ resource "aws_s3_bucket" "example-val" {
 }
 
 # IAM Role
-/*resource "aws_iam_role" "role" {
+resource "aws_iam_role" "role" {
   name = "iam_for_lambda"
 
   assume_role_policy = <<EOF
@@ -49,6 +49,7 @@ EOF
 
 resource "aws_iam_policy" "policy" {
   name        = "s3-policy"
+  path        = "/"
   description = "s3-move-file"
 
   policy = <<EOF
@@ -94,53 +95,6 @@ resource "aws_iam_role_policy_attachment" "test-attach" {
   policy_arn = "aws_iam_policy.policy.arn"
 }
 
-*/
-
-
-resource "aws_iam_role" "role" {
-  name = "test-role"
-
-  assume_role_policy = <<EOF
-    {
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Action": "sts:AssumeRole",
-          "Principal": {
-            "Service": "ec2.amazonaws.com"
-          },
-          "Effect": "Allow",
-          "Sid": ""
-        }
-      ]
-    }
-EOF
-}
-
-resource "aws_iam_policy" "policy" {
-  name        = "test-policy"
-  description = "A test policy"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "ec2:Describe*"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy_attachment" "test-attach" {
-  role       = "aws_iam_role.role.name"
-  policy_arn = "aws_iam_policy.policy.arn"
-}
 
 # Archive a single file.
 

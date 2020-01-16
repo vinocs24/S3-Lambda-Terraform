ffc692b3f49def1f959909416423897148f047f3
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2020-01-16 12:50:56 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -25,6 +25,76 @@ resource "aws_s3_bucket" "example-val" {
   }
 }
 
+#IAM Role:
+
+resource "aws_iam_role" "iam_for_s3_lambda" {
+  name = "iam_for_s3_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "s3_lambda_policy" {
+  name        = "s3_lambda_policy"
+  description = "A test policy"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "ListSourceAndDestinationBuckets",
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListBucket",
+                "s3:ListBucketVersions"
+            ],
+            "Resource": [
+                "arn:aws:s3:::test-bucket-for-transfer-check",
+                "arn:aws:s3:::serverless-demo-s3-bucket"
+            ]
+        },
+        {
+            "Sid": "SourceBucketGetObjectAccess",
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject",
+                "s3:GetObjectVersion"
+            ],
+            "Resource": "arn:aws:s3:::test-bucket-for-transfer-check/*"
+        },
+        {
+            "Sid": "DestinationBucketPutObjectAccess",
+            "Effect": "Allow",
+            "Action": [
+                "s3:PutObject"
+            ],
+            "Resource": "arn:aws:s3:::serverless-demo-s3-bucket/*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role       = aws_iam_role.iam_for_s3_lambda.name
+  policy_arn = aws_iam_policy.s3_lambda_policy.arn
+}
+
+
 # Archive a single file.
 
 data "archive_file" "Test" {

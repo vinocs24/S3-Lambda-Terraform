fc6b5a278ab0c6971d8c200cf479c97ac2e2ac29
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2020-01-16 12:38:05 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,15 +1,27 @@
 import boto3
-import json
 s3 = boto3.resource('s3')
-
-
+clientname=boto3.client('s3')
 def lambda_handler(event, context):
-    bucket = s3.Bucket('my-tf-test-bucket-dev')
-    dest_bucket = s3.Bucket('my-tf-test-bucket-val')
-    print(bucket)
-    print(dest_bucket)
-
-    for obj in bucket.objects():
-        dest_key = obj.key
-        print(dest_key)
-        s3.Object(dest_bucket.name, dest_key).copy_from(CopySource = {'Bucket': obj.bucket_name, 'Key': obj.key})
+    bucket = 'test-bucket-for-transfer-check'
+    try:
+        response = clientname.list_objects(
+            Bucket=bucket,
+            MaxKeys=5
+        )
+        for record in response['Contents']:
+            key = record['Key']
+            copy_source = {
+                'Bucket': bucket,
+                'Key': key
+            }
+            try:
+                destbucket = s3.Bucket('serverless-demo-s3-bucket')
+                destbucket.copy(copy_source, key)
+                print('{} transferred to destination bucket'.format(key))
+            except Exception as e:
+                print(e)
+                print('Error getting object {} from bucket {}. '.format(key, bucket))
+                raise e
+    except Exception as e:
+        print(e)
+        raise e

99d0786c78d0f53609387d23e0d4a7d2e1d20305
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2020-01-16 12:38:03 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -25,77 +25,6 @@ resource "aws_s3_bucket" "example-val" {
   }
 }
 
-# IAM Role
-resource "aws_iam_role" "role" {
-  name = "iam_for_lambda"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-} 
-
-
-resource "aws_iam_policy" "policy" {
-  name        = "s3-policy"
-  path        = "/"
-  description = "s3-move-file"
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "ListSourceAndDestinationBuckets",
-            "Effect": "Allow",
-            "Action": [
-                "s3:ListBucket",
-                "s3:ListBucketVersions"
-            ],
-            "Resource": [
-                "arn:aws:s3:::my-tf-test-bucket-dev",
-                "arn:aws:s3:::my-tf-test-bucket-val"
-            ]
-        },
-        {
-            "Sid": "SourceBucketGetObjectAccess",
-            "Effect": "Allow",
-            "Action": [
-                "s3:GetObject",
-                "s3:GetObjectVersion"
-            ],
-            "Resource": "arn:aws:s3:::my-tf-test-bucket-dev/*"
-        },
-        {
-            "Sid": "DestinationBucketPutObjectAccess",
-            "Effect": "Allow",
-            "Action": [
-                "s3:PutObject"
-            ],
-            "Resource": "arn:aws:s3:::my-tf-test-bucket-val/*"
-        }
-    ]
-}
-EOF
-}
-/*
-resource "aws_iam_role_policy_attachment" "pol" {
-  role       = "aws_iam_role.role.name"
-  policy_arn = "aws_iam_policy.policy.arn"
-}
-*/
-
 # Archive a single file.
 
 data "archive_file" "Test" {
@@ -107,7 +36,7 @@ data "archive_file" "Test" {
 resource "aws_lambda_function" "test_lambda" {
   filename      = "Test.zip"
   function_name = "s3-filemove"
-  role          = aws_iam_role.role.arn
+  role          = aws_iam_role.s3-lambda.arn
   handler       = "Test.lambda_handler"
   source_code_hash = "filebase64sha256(Test.zip)"
 

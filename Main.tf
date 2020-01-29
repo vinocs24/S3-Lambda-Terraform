ca5dfab1e4ddda22c9ab759800d7bf366badd461
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2020-01-29 17:32:13 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -27,6 +27,19 @@ resource "aws_s3_bucket" "example-val" {
   }
 }
 
+#cloudtrail
+
+resource "aws_cloudtrail" "example" {
+  name                          = var.trail_name
+  s3_bucket_name                = aws_s3_bucket.example-CT.id
+  s3_key_prefix                 = "prefix"
+  include_global_service_events = false
+  
+  tags = {
+    Name        = "s3-cloudtrail"
+ 
+  }
+
 # Create S3 Bucket for cloudtrail
 resource "aws_s3_bucket" "example-CT" {
   bucket = var.CT-bucket-name
@@ -69,6 +82,75 @@ POLICY
   }
 }
 
+
+#cloudwatch log group
+
+resource "aws_cloudwatch_log_group" "log_group" {
+  name = "cloudtrail-log-group-tf"
+}
+
+resource "aws_iam_role" "logging_role" {
+  name = "cloudwatch-logging-role-tf"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "cloudtrail.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy" "logging_policy" {
+  name = "cloudwatch-logging-policy-tf"
+  role = "${aws_iam_role.logging_role.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogStream"
+      ],
+      "Resource": [
+        "${aws_cloudwatch_log_group.log_group.arn}"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:PutLogEvents"
+      ],
+      "Resource": [
+        "${aws_cloudwatch_log_group.log_group.arn}"
+      ]
+    }
+  ]
+}
+POLICY
+}
+
+#aws sns topic
+
+resource "aws_sns_topic" "bucket_alerts" {
+  name = "s3-bucket-public-access-alert-tf"
+
+  provisioner "local-exec" {
+    command = "aws sns subscribe --topic-arn ${self.arn} --protocol email --notification-endpoint ${var.alert_email_address}"
+  }
+}
+
+
 #IAM Role:
 
 resource "aws_iam_role" "iam_for_s3_lambda" {
@@ -91,6 +173,68 @@ resource "aws_iam_role" "iam_for_s3_lambda" {
 EOF
 }
 
+resource "aws_iam_role_policy" "lambda_policy" {
+  name = "bucket-alert-lambda-execution-policy-tf"
+  role = aws_iam_role.iam_for_s3_lambda.id
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogStream",
+                "sns:Publish",
+                "logs:PutLogEvents"
+            ],
+            "Resource": [
+                "arn:aws:sns:us-east-1:961508331227:s3-bucket-public-access-alert-tf",
+                "arn:aws:logs:*:*:*"
+            ]
+        },
+        {
+            "Sid": "S3GetBucketACLandPolicy",
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetBucketAcl",
+                "s3:GetBucketPolicy"
+            ],
+            "Resource": "*"
+        },
+		{
+            "Sid": "S3PutBucketACLAccess",
+            "Effect": "Allow",
+            "Action": [
+                "s3:PutBucketAcl",
+                "s3:PutBucketPolicy"
+            ],
+            "Resource": "arn:aws:s3:::*"
+        },
+        {
+            "Sid": "VisualEditor2",
+            "Effect": "Allow",
+            "Action": "logs:CreateLogGroup",
+            "Resource": "arn:aws:logs:*:*:*"
+        },
+        {
+            "Sid": "LambdaBasicExecutionAccess",
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+POLICY
+}
+
+
+/*
 resource "aws_iam_policy" "s3_lambda_policy" {
   name        = "s3_lambda_policy"
   description = "A test policy"
@@ -137,6 +281,8 @@ resource "aws_iam_role_policy_attachment" "test-attach" {
   role       = aws_iam_role.iam_for_s3_lambda.name
   policy_arn = aws_iam_policy.s3_lambda_policy.arn
 }
+*/
+
 
 
 # Archive a single file.
@@ -153,8 +299,12 @@ resource "aws_lambda_function" "test_lambda" {
   role          = aws_iam_role.iam_for_s3_lambda.arn
   handler       = "Test.lambda_handler"
   source_code_hash = "filebase64sha256(Test.zip)"
-
   runtime = "python3.7"
+  environment {
+    variables = {
+      snsTopicArn = aws_sns_topic.bucket_alerts.arn
+    }
+  }
 }
 
 
@@ -162,7 +312,33 @@ resource "aws_lambda_function" "test_lambda" {
 resource "aws_cloudwatch_event_rule" "test-rule" {
   name                = "example-cw-rule"
   description         = "S3 event log"
+  depends_on = [
+    "aws_lambda_function.test_lambda",
+  ]
   
+    event_pattern = <<PATTERN
+{
+  "source": [
+    "aws.s3"
+  ],
+  "detail-type": [
+    "AWS API Call via CloudTrail"
+  ],
+  "detail": {
+    "eventSource": [
+      "s3.amazonaws.com"
+    ],
+    "eventName": [
+      "PutBucketAcl",
+	  "PutBucketPolicy",
+      "CreateBucket"
+    ]
+  }
+}
+PATTERN
+}
+
+/*
   event_pattern = <<PATTERN
 {
   "source": [
@@ -187,6 +363,8 @@ resource "aws_cloudwatch_event_rule" "test-rule" {
 }
 PATTERN
 }
+*/
+
 
 resource "aws_cloudwatch_event_target" "cloud-wtc" {
     rule = aws_cloudwatch_event_rule.test-rule.name
@@ -194,27 +372,10 @@ resource "aws_cloudwatch_event_target" "cloud-wtc" {
     arn = aws_lambda_function.test_lambda.arn
 }
 
-#cloudtrail
-
-resource "aws_cloudtrail" "example" {
-  name                          = var.trail_name
-  s3_bucket_name                = aws_s3_bucket.example-CT.id
-  s3_key_prefix                 = "prefix"
-  include_global_service_events = false
-  /*enable_logging                = true
-  is_multi_region_trail         = false
-  enable_log_file_validation    = true
-
-
-  event_selector {
-    read_write_type           = "All"
-    include_management_events = true
-  }
-*/
-  tags = {
-    Name        = "s3-cloudtrail"
- 
-  }
+resource "aws_lambda_permission" "allow_cloudwatch_event_permission" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.test_lambda.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.test-rule.arn
 }
-
-

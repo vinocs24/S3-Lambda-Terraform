2c1c9c87aa48e961d3b51dedcb44313c6023bcf6
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2020-01-31 15:32:29 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -162,10 +162,10 @@ resource "aws_lambda_function" "test_lambda" {
 resource "aws_cloudwatch_event_rule" "test-rule" {
   name                = "example-cw-rule"
   description         = "S3 event log"
-  
+  /*
 depends_on = [
     aws_lambda_function.test_lambda
-  ]
+  ]*/
 
   event_pattern = <<PATTERN
 {
